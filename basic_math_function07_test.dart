1a124095e496a31db2e9f002a86d4e746d9fbdd8
sanjarbek <sanjarsaidov173@gamil.com>
2022-07-05 19:02:16 +0500

    done

---
+++ basic_math_function07_test.dart
@@ -0,0 +1,9 @@
+import 'package:test/test.dart';
+import '../bin/basic_math_function07.dart';
+import 'dart:math';
+
+void main() {
+  test('Raise cosine of x and the sine of y to the second power', () {
+    expect(func(6234), 4);
+  });
+}
